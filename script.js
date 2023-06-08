682e13ea436aaf8f286e6485ffcfa5381a5210f2
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-06-08 21:23:17 +0530

    feat: Create black trail when squares are clicked

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const gridContainer = document.querySelector('.grid')
-
-for (i = 0; i < 16*16; i++){
+const square = document.createElement('div');
+for (i = 0; i < 16*16; i++){ 
     const square = document.createElement('div');
     square.classList.add('square');
     gridContainer.appendChild(square);
 }
+
+let isDrawing = false;
+
+gridContainer.addEventListener('mousedown', () => {
+    isDrawing = true;
+})
+
+gridContainer.addEventListener('mouseup', () => {
+    isDrawing = false;
+})
+
+gridContainer.addEventListener('mouseleave', () => {
+    isDrawing = false;
+})
+
+gridContainer.addEventListener('mouseover', (e) => {
+    if (isDrawing && e.target.classList.contains('square')){
+        e.target.style.backgroundColor = 'black';
+    }
+})
